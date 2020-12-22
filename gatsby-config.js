cb11148f5f67f011879148912b836239511fca5e
Carlos Hernandez <carlos@techbyte.ca>
2020-12-22 04:01:27 -0700

    Initial commit.

---
+++ gatsby-config.js
@@ -0,0 +1,86 @@
+require(`dotenv`).config({
+  path: `.env`,
+});
+
+const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE;
+
+module.exports = {
+  siteMetadata: {
+    siteTitle: `HurricaneHrndz`,
+    siteTitleAlt: `HurricaneHrndz's TechBlog`,
+    siteHeadline: `Hurricanehrndz's Blog`,
+    siteUrl: `https://hurricanehrndz.ca`,
+    siteDescription: `Thoughts, Experience and Knowledge share on technology related matters.`,
+    siteLanguage: `en`,
+    author: `@hurricanehrndz`,
+  },
+  plugins: [
+    {
+      resolve: `@lekoarts/gatsby-theme-minimal-blog`,
+      // See the theme's README for all available options
+      options: {
+        navigation: [
+          {
+            title: `Blog`,
+            slug: `/blog`,
+          },
+          {
+            title: `About`,
+            slug: `/about`,
+          },
+        ],
+        externalLinks: [
+          {
+            name: `Twitter`,
+            url: `https://twitter.com/hurricanehrndz`,
+          },
+          {
+            name: `Instagram`,
+            url: `https://www.instagram.com/hurricanehrndz/`,
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.GOOGLE_ANALYTICS_ID,
+      },
+    },
+    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `HurricaneHrndz's WordStorm - Tech Blog`,
+        short_name: `HurricaneHrndz`,
+        description: `Technology Centric Blog - Personal WordStorm`,
+        start_url: `/`,
+        background_color: `#fff`,
+        theme_color: `#6B46C1`,
+        display: `standalone`,
+        icons: [
+          {
+            src: `/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+        ],
+      },
+    },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-netlify`,
+    shouldAnalyseBundle && {
+      resolve: `gatsby-plugin-webpack-bundle-analyser-v2`,
+      options: {
+        analyzerMode: `static`,
+        reportFilename: `_bundle.html`,
+        openAnalyzer: false,
+      },
+    },
+  ].filter(Boolean),
+};
